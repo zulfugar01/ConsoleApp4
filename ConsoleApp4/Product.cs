51cacdbb1d11b32b932a110e8beb64997b2df7d1
zulfugar01 <izulfuqar6@gmail.com>
2021-03-12 03:20:44 -0800

    Product properties and constructor added

--- ConsoleApp4/Product.cs
+++ ConsoleApp4/Product.cs
@@ -8,5 +8,23 @@ namespace ConsoleApp4
 {
     class Product
     {
+        public Product[] products = new Product[0];
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public enum Category { }
+        public int Number { get; set; }
+        public readonly int Id;
+        private static int _id = 0;
+        public Product()
+        {
+            _id++;
+            Id = _id;
+        }
+        public Product(string name, double price, int number)
+        {
+            Name = name;
+            Price = price;
+            Number = number;
+        }
     }
 }
